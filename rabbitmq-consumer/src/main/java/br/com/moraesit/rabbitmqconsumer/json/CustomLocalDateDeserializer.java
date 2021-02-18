825e474e988c0b0b65164f33572c38c7239e9a2c
casjom <joao.moraes@capela.com.br>
2021-02-18 14:39:02 -0300

    Adding LocalDateDeserializer

---
+++ rabbitmq-consumer/src/main/java/br/com/moraesit/rabbitmqconsumer/json/CustomLocalDateDeserializer.java
@@ -0,0 +1,21 @@
+package br.com.moraesit.rabbitmqconsumer.json;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CustomLocalDateDeserializer extends StdDeserializer<LocalDate> {
+    public CustomLocalDateDeserializer() {
+        super(LocalDate.class);
+    }
+
+    @Override
+    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        return LocalDate.parse(jsonParser.readValueAs(String.class), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+}
