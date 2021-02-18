3e1e77550da65c9ddfc5b43c4d33fdf4d3013503
casjom <joao.moraes@capela.com.br>
2021-02-18 13:08:45 -0300

    FixedRateProducer

--- rabbitmq-producer/src/main/java/br/com/moraesit/rabbitmqproducer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/br/com/moraesit/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,17 +1,25 @@
 package br.com.moraesit.rabbitmqproducer;
 
-import br.com.moraesit.rabbitmqproducer.producer.HelloRabbitProducer;
+import br.com.moraesit.rabbitmqproducer.producer.FixedRateProducer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RabbitmqProducerApplication implements CommandLineRunner {
 
-	private final HelloRabbitProducer helloRabbitProducer;
+//	private final HelloRabbitProducer helloRabbitProducer;
+//
+//	public RabbitmqProducerApplication(HelloRabbitProducer helloRabbitProducer) {
+//		this.helloRabbitProducer = helloRabbitProducer;
+//	}
 
-	public RabbitmqProducerApplication(HelloRabbitProducer helloRabbitProducer) {
-		this.helloRabbitProducer = helloRabbitProducer;
+	private final FixedRateProducer fixedRateProducer;
+
+	public RabbitmqProducerApplication(FixedRateProducer fixedRateProducer) {
+		this.fixedRateProducer = fixedRateProducer;
 	}
 
 	public static void main(String[] args) {
@@ -20,6 +28,6 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		helloRabbitProducer.sendHello("Rabbit MQ");
+		//helloRabbitProducer.sendHello("Rabbit MQ");
 	}
 }
---
+++ rabbitmq-producer/src/main/java/br/com/moraesit/rabbitmqproducer/producer/FixedRateProducer.java
@@ -0,0 +1,27 @@
+package br.com.moraesit.rabbitmqproducer.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+    private int i = 0;
+
+    private Logger log = LoggerFactory.getLogger(FixedRateProducer.class);
+
+    public FixedRateProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage() {
+        i++;
+        log.info("i is " + i);
+        rabbitTemplate.convertAndSend("course.fixedrate", "Fixed rate " + i);
+    }
+}
