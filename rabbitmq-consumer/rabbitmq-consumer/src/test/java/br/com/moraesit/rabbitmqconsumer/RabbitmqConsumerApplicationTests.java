e6b82f41caadb6b4d462a682436f9d78947922e7
João Pedro <joao.moraes@capela.com.br>
2021-02-18 11:50:16 -0300

    Setup RabbitMQ producer and consumer project

---
+++ rabbitmq-consumer/rabbitmq-consumer/src/test/java/br/com/moraesit/rabbitmqconsumer/RabbitmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.rabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
